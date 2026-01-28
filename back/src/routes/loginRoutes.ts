cbbf21cc5eecc5a065ad77c9f81b67b0c519d1eb
Alisson Almeida de Oliveira <alissonttfire12@gmail.com>
2026-01-28 04:31:15 -0300

    Remove validação de token da rota de login

--- back/src/routes/loginRoutes.ts
+++ back/src/routes/loginRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { LoginController } from "../controllers/LoginController";
-import { validateToken } from "../middlewares/authMiddleware";
 
 export const loginRoutes = Router();
-loginRoutes.post("/", validateToken, LoginController.login);
+
+loginRoutes.post("/", LoginController.login);
